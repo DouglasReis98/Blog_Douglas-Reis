a38cb149612b0b5224b7924aff05e65ab6b328bf
DouglasReis98 <y*********@gmail.com>
2025-03-04 11:34:38 -0300

    google_adsense

--- src/components/AbaLateral.js
+++ src/components/AbaLateral.js
@@ -1,49 +1,44 @@
-import { useState } from 'react'
-import style from '../components/css/AbaLateral.module.css'
-import { db } from '../firebaseConfig/firebaseConfig'
-import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore'
-import { useEffect } from 'react'
+import { useState } from "react";
+import style from "../components/css/AbaLateral.module.css";
+import { db } from "../firebaseConfig/firebaseConfig";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { useEffect } from "react";
 
 const AbaLateral = () => {
+  const [data, setData] = useState([]);
 
-	const [data, setData] = useState([]);
+  useEffect(() => {
+    const postsPerViews = async () => {
+      const postsRef = collection(db, "postagens");
+      const q = query(postsRef, orderBy("views", "asc"), limit(5));
+      const querySnapshot = await getDocs(q);
 
+      const posts = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
 
-	useEffect(() => {
+      setData(posts);
+    };
 
-		const postsPerViews = async () => {
-		const postsRef = collection(db, 'postagens');
-		const q = query(postsRef, orderBy('views', 'asc'), limit(5))
-		const querySnapshot = await getDocs(q)
-
-		const posts = querySnapshot.docs.map((doc) => ({
-			id: doc.id,
-			...doc.data()
-		}))
-
-		setData(posts);;
-	}
-
-	postsPerViews()
-	}, [])
-		console.log(data)
+    postsPerViews();
+  }, []);
 
   return (
     <aside className={style.aba_lateral}>
-				<div className={style.ultimos_artigos}>
-					<h3>Artigos Mais Lidos</h3>
-					<br />
-					{data.map((d) => (
-						<p key={d.id}>{d.titulo}</p>
-					)) }
-					
-				</div>
-				<div className={style.publicidade}>
-					<h3>Publicidade</h3>
+      <div className={style.ultimos_artigos}>
+        <h3>Artigos Mais Lidos</h3>
+        <br />
+        {data.map((d) => (
+          <p key={d.id}>{d.titulo}</p>
+        ))}
+      </div>
+      <div className={style.publicidade}>
+        <h3>Publicidade</h3>
+        
+      </div>
+    </aside>
+  );
+};
 
-				</div>
-			</aside>
-  )
-}
-
-export default AbaLateral
+export default AbaLateral;
