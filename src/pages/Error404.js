26b06aa8b1c01b4b3d72eb0e682ebc89fe879e7e
DouglasReis98 <y*********@gmail.com>
2025-03-07 19:35:50 -0300

    error404_title

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import style from './css/Error404.module.css'
+import { useEffect } from 'react';
 const Error404 = () => {
+
+  useEffect(() => {
+      document.title = `Erro 404 - Página Não Encontrada - Blog Douglas Reis`
+    }, []);
+
   return (
     <div id={style.principal}>
       <h1>Error 404 - Página Não Encontrada!</h1>
       <h2>Opa! Parece que você está perdido!</h2>
-      <h3><a href="/blog">Clique aqui para voltar a página principal</a></h3>
+      <h3><a href="/">Clique aqui para voltar a página principal</a></h3>
     </div>
   )
 }
